fcfc40813e73c30ef56c481c6f9346c7e4aab18f
Amir Diyanatdar <159172919+Amir-Diyanatdar@users.noreply.github.com>
2025-10-16 23:22:00 +0330

    Day002

---
+++ MyFirstRestApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet("sayHello")]
+        public string HelloWorld()
+        {
+            string firstMessage = "Hello, this is my first restful api!";
+            return firstMessage;
+        }
+        //==========================================================
+        [HttpGet("sayHelloAction")]
+        public IActionResult sayHelloAction()
+        {
+            string secondMessage = "This is my second message";
+            return Ok(secondMessage);
+        }
+        //==========================================================
+        [HttpGet("sayHelloFirstName")]
+        public IActionResult sayHelloFirstName(string firstName)
+        {
+            string thirdMessage = $"Hi! {firstName}, this is my third message";
+            return Ok(thirdMessage);
+        }
+        //============================================================
+        [HttpGet("sayHelloFullName")]
+        public IActionResult sayHelloFullName(string firstName, string LastName)
+        {
+            string forthMessage = $"hi! {firstName} {LastName}, this is my forth Message";
+            return Ok(forthMessage);
+        }
+        //============================================================
+    }
+}
