268b7e961a5673c15e0b2dbe9ba3c13f0f0927ff
Amir Diyanatdar <159172919+Amir-Diyanatdar@users.noreply.github.com>
2025-10-24 00:08:35 +0330

    Day005

---
+++ MyFirstRestApi/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("Calculate")]
+        public IActionResult Calculate(decimal firstNumber, decimal SecondNumber, string ActionType)
+        {
+            decimal result;
+            //=========================
+            switch (ActionType)
+            {
+                case "+":
+                    result = firstNumber + SecondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - SecondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * SecondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / SecondNumber;
+                    break;
+                default:
+                    result = 0;
+                    return NotFound();
+                    
+            }
+            return Ok(result);
+
+        }
+    }
+}
